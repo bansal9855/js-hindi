ef26627aff4fd6f7495b578dce5c1bd35505cb85
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-04-13 16:09:10 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score="33abc"
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //nan- not a number
+// let scale=null
+// let value=Number(null)
+// console.log(value);//0
+// let score1=undefined
+// let value2=Number(score1)
+// console.log(value2) //nan
+// let score3=""
+// let value3=Boolean(score3)
+// console.log(value3);//false
+// let score4="hello"
+// let value4=Boolean(score4)
+// console.log(value4);//true
+
+
+//  ******************operations****************
+// let a="hello"
+// let b="world"
+// console.log(a+b);
+// let value=-a
+// console.log(value);
+// let a1="5"
+// let value1=-a1
+// console.log(value1);
+
+console.log(1+"2") //12
+console.log("1"+"2") //12
+console.log("1"+2) //12
+console.log(1+2+"2")  //32
+console.log("1"+2+2) //122
+console.log(1+"a")         //1a
+console.log("a"+"b"+1)   //ab1
+console.log(1+"a"+10)     //1a10
+console.log(3+4*5%3);
