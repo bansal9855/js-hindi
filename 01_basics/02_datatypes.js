b83e9aa8d79fa0c277e574d812980e8e7635b66f
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-04-13 06:51:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as new version
+// alert(3+3) //we are using nodejs and not browser so it will give error
+
+
+//datatypes
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol => unique
+
+console.log(typeof "hi lovely") //string
+console.log(typeof null) //object  but should give null
+console.log(typeof undefined) //undefined
