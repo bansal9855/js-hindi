ae55db876fe41e09bd7bff80c7b8553495cf362a
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-04-18 05:22:41 +0000

    change in location

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+//primitive
+//string number bigint,boolean,undefined null symbol
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid)
+const bignumber=3444567895678n
+//non primitive arrays,objects,functions
+const heros=["ironman","hulk","thor"] //array
+console.log(heros)
+let myobj={         //object
+    name:"lovely", 
+    age:21,
+}
+const myfunction=function(){
+    console.log("hello world");
+}
+myfunction();
