a62d1be559147c7452146ef2889b52c41a2d4fe7
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-04-19 05:04:13 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,17 +1,34 @@
 //primitive
 //string number bigint,boolean,undefined null symbol
-const id=Symbol('123')
-const anotherid=Symbol('123')
-console.log(id===anotherid)
-const bignumber=3444567895678n
-//non primitive arrays,objects,functions
-const heros=["ironman","hulk","thor"] //array
-console.log(heros)
-let myobj={         //object
-    name:"lovely", 
-    age:21,
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id===anotherid)
+// const bignumber=3444567895678n
+// //non primitive arrays,objects,functions
+// const heros=["ironman","hulk","thor"] //array
+// console.log(heros)
+// let myobj={         //object
+//     name:"lovely", 
+//     age:21,
+// }
+// const myfunction=function(){
+//     console.log("hello world");
+// }
+// myfunction();
+
+
+
+//+++++++++++++++++++++++++++++++++++++
+let myYoutubeName="bansal9855.com"
+let anotherName=myYoutubeName
+anotherName="bansal2905"
+console.log(myYoutubeName)
+console.log(anotherName);
+let userone={
+    email:"user@google.com",
+    upi:"user@sbi",
 }
-const myfunction=function(){
-    console.log("hello world");
-}
-myfunction();
+let usertwo=userone
+usertwo.email="bansal@google.com"
+console.log(userone.email);
+console.log(usertwo.email);
