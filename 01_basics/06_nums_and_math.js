94ab4026cb0b739673d96c9dccba5ec402cbaab0
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-05-23 10:09:59 +0000

    save changes

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -18,3 +18,12 @@ console.log(Math.abs(-4))
 console.log(Math.round(2.6))
 console.log(Math.ceil(4.3))
 console.log(Math.floor(4.9))
+console.log(Math.min(4,5,3,8))
+console.log(Math.max(4,5,3,8))
+console.log(Math.random())  //value always comes between 0 and 1
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1) // to avoid values with zero
+
+const min = 10
+const max =20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
