10d9d07232b110ed7b8765227258b181871242ba
bansal9855 <140716270+bansal9855@users.noreply.github.com>
2024-06-06 14:51:37 +0000

    save

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,34 @@
 //if
 const isuserloogedin=true
 const temperature=41
-if(2!=3){
-    console.log("executed");
+// if(2!=3){
+//     console.log("executed");
+// }
+// else{
+//     console.log("not executed");
+// }
+const balance=1000
+if(balance>500) console.log("test");
+// if(balance>500) console.log("test"),
+// console.log("test2"); //not a good practice
+
+// if(balance<500){
+//     console.log("less than 500");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+const userloggedin=true
+const debitcard= true
+const loggedinfromemail=true
+const loggedinfromgoogle=false
+
+if(userloggedin&&debitcard ){
+    console.log("allow to  buy course");
+}
+if(loggedinfromemail || loggedinfromgoogle){
+    console.log("user logged in");
 }
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,31 @@
+// let useremail;
+// if(useremail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("don't have user email");
+// }
+// const useremail="lovely"
+// if(useremail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("don't have user email");
+// }
+// const useremail=""
+// if(useremail){
+//     console.log("got user email");
+// }
+// else{
+//     console.log("don't have user email");
+// }
+const useremail=[]
+if(useremail){
+    console.log("got user email");
+}
+else{
+    console.log("don't have user email");
+}
+//falsy values
+// false,0,-0,bigint 0n,"",null,undefined,NaN
+// all other values are truthy values
